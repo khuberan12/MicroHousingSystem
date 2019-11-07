11fc5289218d6621c416fc66185f5d91565253f6
khuberan12 <44001926+khuberan12@users.noreply.github.com>
2019-11-08 02:55:38 +0800

    dialogbox

--- app/src/main/java/com/example/microhousingsystem/MainActivity.java
+++ app/src/main/java/com/example/microhousingsystem/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.microhousingsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignup = (Button) findViewById(R.id.btnSignup);
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
     }
-}
+
+        public void openDialog(){
+            SignUpDialog signUpDialog = new SignUpDialog();
+            signUpDialog.show(getSupportFragmentManager(),"Sign Up");
+        }
+    }
---
+++ app/src/main/java/com/example/microhousingsystem/SignUpDialog.java
@@ -0,0 +1,39 @@
+package com.example.microhousingsystem;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class SignUpDialog extends AppCompatDialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Please select your position: ")
+                .setMessage("Are you a Housing Officer or an Applicant?")
+            .setPositiveButton("Applicant", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int id) {
+                    Intent x = new Intent(SignUpDialog.this.getActivity(), SignUpAP.class);
+                    startActivity(x);
+
+                }
+            })
+                .setNegativeButton("Housing Officer", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int id) {
+                        Intent y = new Intent(SignUpDialog.this.getActivity(), SignUpHO.class);
+                        startActivity(y);
+                    }
+                });
+
+
+
+        return builder.create();
+    }
+}
