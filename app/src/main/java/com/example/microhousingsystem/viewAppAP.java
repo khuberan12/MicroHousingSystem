cea8c6c62102c660f8830970506c926a5e000f72
khuberan12 <44001926+khuberan12@users.noreply.github.com>
2019-11-13 17:49:49 +0800

    dialogbox back

---
+++ app/src/main/java/com/example/microhousingsystem/viewAppAP.java
@@ -0,0 +1,14 @@
+package com.example.microhousingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class viewAppAP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_app_ap);
+    }
+}
