b9fd38596db0693041a22b05e08cd47d2ed9dbaa
AaronKajenderan <aaronriboy@gmail.com>
2019-11-14 18:50:46 +0800

    database

---
+++ app/src/main/java/com/example/microhousingsystem/Login.java
@@ -0,0 +1,4 @@
+package com.example.microhousingsystem;
+
+public class Login {
+}
--- app/src/main/java/com/example/microhousingsystem/MainActivity.java
+++ app/src/main/java/com/example/microhousingsystem/MainActivity.java
@@ -1,56 +1,85 @@
 package com.example.microhousingsystem;
-
+import android.support.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
     EditText usernameEditText;
     EditText passwordEditText;
-    SqliteHelper sqliteHelper;
+    FirebaseAuth mFirebaseAuth;
+
     Button btnSignup;
 
-    public void Login (View view){
+    public void Login (View  view){
 
         String username = usernameEditText.getText().toString();
         String Password = passwordEditText.getText().toString();
 
-        //Authenticate user
-        User currentUser = sqliteHelper.Authenticate(new User(null, username, Password));
-
-        //Check Authentication is successful or not
-        if (currentUser != null) {
-            Toast.makeText(this, "Loged In", Toast.LENGTH_SHORT).show();
-            //User Logged in Successfully Launch You home screen activity
-
-        } else {
-            Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show();
-            //User Logged in Failed
-        }
-
-
     }
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnSignup = (Button) findViewById(R.id.btnSignup);
+        //To view housing officer or residence
+        btnSignup =  findViewById(R.id.btnSignup);
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openDialog();
             }
         });
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+
+        passwordEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (email.isEmpty()){
+                    usernameEditText.setError("Please enter email ID");
+                    usernameEditText.requestFocus();
+                }
+                else if (password.isEmpty()){
+                    passwordEditText.setError("Please enter password");
+                    passwordEditText.requestFocus();
+                }
+                else if (email.isEmpty() && password.isEmpty()){
+                    Toast.makeText(MainActivity.this,"Field are Empty!", Toast.LENGTH_SHORT).show();
+                }
+                else if (!(email.isEmpty() && password.isEmpty())){
+                    mFirebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(MainActivity.this, new)
+                }
+            }
+        });
+
+
+
+
+
+
+
+
+
+
     }
 
     public void openDialog() {
