c8d4cf6f3704c2996e7813cd47b30d2179f21b57
khuberan12 <44001926+khuberan12@users.noreply.github.com>
2019-11-21 18:23:34 +0800

    Merge branch 'master' of https://github.com/khuberan12/MicroHousingSystem
    # Conflicts:
    #	app/src/main/res/layout/activity_sign_up_ap.xml

--- app/src/main/java/com/example/microhousingsystem/LoginHO.java
+++ app/src/main/java/com/example/microhousingsystem/LoginHO.java
@@ -3,7 +3,6 @@ package com.example.microhousingsystem;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroupOverlay;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
