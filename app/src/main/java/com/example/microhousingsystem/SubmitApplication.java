d2b162fd0992e3f6c48c55c435f775af83665177
khuberan12 <44001926+khuberan12@users.noreply.github.com>
2019-11-25 20:08:12 +0800

    submit application

--- app/src/main/java/com/example/microhousingsystem/SubmitApplication.java
+++ app/src/main/java/com/example/microhousingsystem/SubmitApplication.java
@@ -2,14 +2,32 @@ package com.example.microhousingsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SubmitApplication extends AppCompatActivity {
+
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_application);
 
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openViewApp();
+            }
+        });
+
+    }
+
+    public void openViewApp () {
+        Intent i = new Intent(this, ViewApplication.class);
+        startActivity(i);
     }
 }
