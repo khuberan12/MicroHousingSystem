dd36a74f141be1f21c5fd33e7048e0610d8784a7
khuberan12 <44001926+khuberan12@users.noreply.github.com>
2019-11-25 20:34:10 +0800

    submit application

--- app/src/main/java/com/example/microhousingsystem/SubmitApplication.java
+++ app/src/main/java/com/example/microhousingsystem/SubmitApplication.java
@@ -6,28 +6,53 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SubmitApplication extends AppCompatActivity {
 
+    SqliteHelper sqliteHelper;
     private Button btnSubmit;
+    private EditText enter_date;
+    private EditText enter_month;
+    private EditText enter_year;
+
+    Application application;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_application);
 
-        btnSubmit = findViewById(R.id.btnSubmit);
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openViewApp();
-            }
-        });
 
+        sqliteHelper = new SqliteHelper(SubmitApplication.this);
+        enter_date = findViewById(R.id.enter_date);
+        enter_month = findViewById(R.id.enter_month);
+        enter_year = findViewById(R.id.enter_year);
+
+        Intent intent = getIntent();
+        int id = intent.getIntExtra("id", -1);
+        if (id != -1) {
+            application = sqliteHelper.getApplication(id);
+
+            enter_date.setText(application.getApplicationDate());
+            enter_month.setText(application.getRequiredMonth());
+            enter_year.setText(application.getRequiredYear());
+
+            btnSubmit = findViewById(R.id.btnSubmit);
+            btnSubmit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    openViewApp();
+                }
+            });
+
+        }
     }
 
-    public void openViewApp () {
+    public void openViewApp() {
         Intent i = new Intent(this, ViewApplication.class);
         startActivity(i);
+        Toast.makeText(this, "Your application has been submitted", Toast.LENGTH_SHORT).show();
     }
 }
